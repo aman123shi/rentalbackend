5dd3b510f5d0285be3eff4af0d24e814d165b80c
Amanuel Shiferaw <123aman64@gmail.com>
2022-06-09 22:22:13 +0300

    houseCategory model fixed

--- models/houseCategory.js
+++ models/houseCategory.js
@@ -1,17 +1,14 @@
 const mongoose = require("mongoose");
-    const houseCategoryModelSchema = new mongoose.Schema({
-        name: {
-            type: String,
-            required: true,
-            min: 1,
-            max: 40
-        },
-        bedrooms:Boolean,
-        bathrooms:Boolean
-      
-    
-    });
+const houseCategoryModelSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    min: 1,
+    max: 40,
+  },
+  bedrooms: Boolean,
+  bathrooms: Boolean,
+});
 
-const HouseModel = mongoose.model("HouseModel",carModelSchema);
-module.exports.House = HouseModel;
-module.exports.houseModelSchema = houseModelSchema;
+const HouseCategory = mongoose.model("HouseCategory", houseCategoryModelSchema);
+module.export.HouseCategory = HouseCategory;
