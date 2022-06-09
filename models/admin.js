366d82b87fdd4d230a455c11e3e367386b6409ae
Amanuel Shiferaw <123aman64@gmail.com>
2022-06-09 23:44:01 +0300

    admin model added

---
+++ models/admin.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const adminSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+    min: 1,
+    max: 255,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    min: 1,
+    max: 500,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    required: true,
+    type: String,
+  },
+});
+
+const Admin = mongoose.model("Admin", adminSchema);
+module.export = Admin;
