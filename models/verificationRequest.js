c69ceea4380ad4ff5790a35d5aed1c2f3a2d2c64
Amanuel Shiferaw <123aman64@gmail.com>
2022-06-10 15:18:13 +0300

    verification model implemented

---
+++ models/verificationRequest.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const verificationRequestSchema = new mongoose.Schema({
+  properties: [
+    {
+      id: mongoose.Types.ObjectId,
+      propertyType: String, //House or Car
+    },
+  ],
+
+  renter: { type: mongoose.Types.ObjectId, ref: "Renter" },
+  city: String,
+  verifiedBy: { type: mongoose.Types.ObjectId, ref: "Agent" },
+  subCity: { id: mongoose.Types.ObjectId, name: Sting },
+  status: String,
+  description: String,
+  companyType: String,
+  paymentStatus: String,
+  createdAt: { type: Date, default: Date.now, immutable: true },
+  updatedAt: { type: Date, default: Date.now },
+});
+
+const VerificationRequest = mongoose.model(
+  "VerificationRequest",
+  verificationRequestSchema
+);
+module.export = VerificationRequest;
