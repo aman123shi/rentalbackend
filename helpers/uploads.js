15e0e2beaf669441219868af1835767ae14ecab3
Amanuel Shiferaw <123aman64@gmail.com>
2022-06-14 22:53:06 +0300

    multer image upload added

---
+++ helpers/uploads.js
@@ -0,0 +1,18 @@
+const multer = require("multer");
+const path = require("path");
+
+const storageEngine = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/images/");
+  },
+
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
+
+let upload = multer({ storage: storageEngine });
+module.exports = upload;
