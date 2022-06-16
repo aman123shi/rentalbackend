1f9edb706a1f3da3bf92d3bdb89568f1feca5eaf
Amanuel Shiferaw <123aman64@gmail.com>
2022-06-16 10:38:32 +0300

    super admin guard added

---
+++ middlewares/super-admin-guard.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken");
+const config = require("config");
+module.exports = async (req, res, next) => {
+  let token = req.header("x-auth-token");
+  if (!token)
+    return res.status(401).send("Access Denied:token should be provided");
+  try {
+    let decode = jwt.verify(token, config.get("jwtPrivateKey"));
+    req.user = decode;
+
+    if (!(req.user.userType && req.user.userType == "admin"))
+      return res.status(401).send("Access Denied: insufficient privilege");
+
+    next();
+  } catch (error) {
+    return res.status(401).send("Access Denied:token Invalid");
+  }
+};
