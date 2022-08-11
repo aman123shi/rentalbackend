759ff5989eb58d3af616246fe806e452e341b588
Amanuel Shiferaw <123aman64@gmail.com>
2022-08-11 06:08:00 +0300

    ADD Axios-Retry Package and Payment Sucess

--- routes/bookings.js
+++ routes/bookings.js
@@ -11,7 +11,21 @@ const Booking = require("../models/booking");
 const guard = require("../middlewares/guard");
 const adminGuard = require("../middlewares/adminGuard");
 const House = require("../models/house");
-
+const jwt = require("jsonwebtoken");
+const axios = require("axios").default;
+const axiosRetry = require("axios-retry");
+let CHAPA_SECRET_KEY = process.env.CHAPA_SECRET_KEY;
+let config = {
+  headers: {
+    Authorization: "Bearer " + CHAPA_SECRET_KEY,
+  },
+};
+axiosRetry(axios, {
+  retries: 10,
+  retryDelay: (retryCount) => {
+    return 500;
+  },
+});
 //GET api/bookings
 router.get("/", adminGuard, async (req, res) => {
   let subCity,
@@ -29,20 +43,90 @@ router.get("/", adminGuard, async (req, res) => {
     .limit(limit); // items per page
   res.send({ success: true, body: bookings });
 });
+//POST api/bookings/init
+router.post("/init", guard, async (req, res) => {
+  let property = null;
+  let tenantId = req.user.id;
 
-//POST api/bookings
-
-router.post("/", adminGuard, async (req, res) => {
-  let property,
-    query = {},
-    findPostQuery = {};
   const error = validateBooking(req.body);
+
   if (error) {
     res.status(400).send({ success: false, message: error.message });
     return;
   }
+
+  let tenant = await Renter.findById(tenantId);
+
   let propertyId = req.body.property;
+  let propertyType = req.body.propertyType;
   if (req.body.propertyType == "House") {
+    property = await House.findById(propertyId);
+  } else {
+    property = await Car.findById(propertyId);
+  }
+
+  if (!property)
+    return res
+      .status(400)
+      .send({ success: false, message: "can't find the property" });
+
+  let data = {
+    tenantId,
+    propertyType,
+    propertyId: property._id,
+    price: property.price,
+    date: Date.now(),
+  };
+
+  let tx_ref = jwt.sign(data, process.env.Ethio_Rental_Private_Key).toString();
+  //tx_ref = "aaaaaa" + Date.now();
+  console.log(tx_ref);
+  try {
+    let customization = {};
+    let result = await axios.post(
+      "https://api.chapa.co/v1/transaction/initialize",
+      {
+        amount: property.price,
+        currency: "ETB",
+        email: "user@mail.com",
+        first_name: tenant.firstName,
+        last_name: tenant.lastName,
+        tx_ref: tx_ref,
+        //localhost should be changed in deployment or Integration
+        callback_url:
+          "http://localhost:3000/api/bookings/success?tx_ref=" + tx_ref,
+      },
+      config
+    );
+
+    //console.log(result.data);
+
+    //returning back the checkout url to Frontend
+
+    res.send(result.data);
+  } catch (error) {
+    console.log(error);
+    res.send("error message " + error);
+  }
+  //
+});
+
+//POST api/bookings/success
+
+router.get("/success", async (req, res) => {
+  let property,
+    query = {},
+    findPostQuery = {};
+  const payload = req.query.tx_ref;
+  console.log("decoded token");
+  console.log(jwt.verify(payload, process.env.Ethio_Rental_Private_Key));
+  let transactionData = jwt.verify(
+    payload,
+    process.env.Ethio_Rental_Private_Key
+  );
+
+  let propertyId = transactionData.propertyId;
+  if (transactionData.propertyType == "House") {
     property = await House.findById(propertyId);
     query.house = propertyId;
     query.propertyCity = property.city;
@@ -55,41 +139,47 @@ router.post("/", adminGuard, async (req, res) => {
     findPostQuery.car = propertyId;
   }
   if (!property)
-    return res
-      .status(400)
-      .send({ success: false, message: "can't find the property" });
+    return res.status(400).send({
+      success: false,
+      message: "Internal Server error can't find the property",
+    });
 
-  let tenant = await Renter.findById(req.user.id);
+  let tenant = await Renter.findById(transactionData.tenantId);
   let renter = await Renter.findById(property.owner);
-  if (Number.parseFloat(tenant.balance) < Number.parseFloat(property.price)) {
+
+  if (
+    Number.parseFloat(transactionData.price) < Number.parseFloat(property.price)
+  ) {
     return res.send({
       success: false,
       message: "insufficient balance please re-charge",
     });
   }
-  // subtract balance from tenant account
-  tenant.balance -= property.price;
+
   //assign other properties to fields
 
-  query.propertyType = req.body.propertyType;
+  query.propertyType = transactionData.propertyType;
   query.renter = renter._id;
-  query.tenant = req.user.id;
+  query.tenant = transactionData.tenantId;
   query.amount = property.price;
-  query.status = "pending";
+  query.status = "pending"; //pending,canceled,confirmed
   query.paymentStatus = "payed";
 
   let booking = new Booking(query);
   await booking.save();
   //removing from active listing
   await Post.findOneAndUpdate(findPostQuery, { status: "inactive" });
-  return res.send({ success: true, body: booking });
+  res.send(
+    '<h3 style="color:green">Payment successful go to my-booking for more Info </h3>'
+  );
+  //return res.send({ success: true, body: booking });
 });
 
 //PUT api/bookings/cancel/:id
 router.put("/cancel/:id", validateObjectId, async (req, res) => {
   let booking = await Booking.findOneAndUpdate(
     { _id: req.params.id, tenant: req.user.id, status: "pending" },
-    { status: "canceled", updatedAt: Date.now() },
+    { status: "canceled" },
     { new: true }
   );
   if (!booking) return sendMessage(false, "can't find booking");
@@ -112,13 +202,13 @@ router.put("/confirm/:id", validateObjectId, async (req, res) => {
   }
   let booking = await Booking.findOneAndUpdate(
     { _id: req.params.id, tenant: req.user.id },
-    { status: "confirmed", updatedAt: Date.now() },
+    { status: "confirmed" },
     { new: true }
   );
   if (!booking) return sendMessage(false, "can't find booking");
   //add payment to renter balance
   let renter = Renter.findById(booking.renter);
-  renter.balance += booking.price - booking.price * 0.05; //cut 5% commission
+  renter.balance += booking.price - booking.price * 0.02; //cut 2% commission
 
   if (booking.propertyType == "House") {
     //delete the post
